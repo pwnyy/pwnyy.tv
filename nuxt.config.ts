e240ea337cc45b70d88c18560a7cfa75285b598d
pwnyy <hubertytom@hotmail.com>
2024-06-18 06:21:37 +0200

    Update nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,5 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
-import TwitchPlayerWithChat from '~/components/TwitchPlayerChat.vue';
+//import TwitchPlayerWithChat from '~/components/TwitchPlayerChat.vue';
 export default defineNuxtConfig({
   devtools: { enabled: true },
   extends: ['shadcn-docs-nuxt'],
